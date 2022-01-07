d58463fddd693789bbf71ae7e1a148313a884063
Bhupinder <bhupi98.bs@gmail.com>
2022-01-06 22:10:04 -0800

    have control over quadrants

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let gameData = [9,8,7,6,5,4,3,2,1];
 const gridDiv= document.querySelector('#grid');
-let boxList=gridDiv.querySelectorAll(".box1");
+let boxList=gridDiv.querySelectorAll(".quadrants");
 gridDiv.addEventListener('click',function(e){
     console.log(e.path[0].className);
 });
-
+//this works!
 function displayData(gameData){
     for(let i=0;i<gameData.length;i++){
-
+        let nodeQ= gridDiv.querySelector(`#${boxList[i].id}`);
+        nodeQ.innerHTML=`${gameData[i]}`;
     }
 }
