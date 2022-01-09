7e11e364b7004beff626597904018afe3171d8f2
Bhupinder <bhupi98.bs@gmail.com>
2022-01-08 21:41:51 -0800

    Grid can be clicked and interacted with

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-let gameData = [9,8,7,6,5,4,3,2,1];
+let gameData = [];
 const gridDiv= document.querySelector('#grid');
 let boxList=gridDiv.querySelectorAll(".quadrants");
-gridDiv.addEventListener('click',function(e){
-    console.log(e.path[0].className);
-});
-//this works!
-function displayData(gameData){
-    for(let i=0;i<gameData.length;i++){
-        let nodeQ= gridDiv.querySelector(`#${boxList[i].id}`);
-        nodeQ.innerHTML=`${gameData[i]}`;
-    }
+//Display data updates the grid layout when called
+function displayData(gameData,gridBlockIndex){
+    let nodeQ= gridDiv.querySelector(`#${boxList[gridBlockIndex].id}`);
+    nodeQ.innerHTML=`${gameData[gridBlockIndex]}`;
 }
+
+gridDiv.addEventListener('click',function(e){
+    let clickId=e.path[0].id;
+    let boxNumber=0;
+    let clickIndex=0;
+    if(clickId == 'grid') return; //if the grid element is clicked on ignore. 
+    clickIndex=Number(clickId.slice(3))-1; //! need to offset by 1. Should have indexed grid by 0
+    console.log(clickIndex);
+    gameData[clickIndex]='O';
+    console.log(gameData);
+    displayData(gameData,clickIndex);
+});
+
+//Displays random numbers in the grid
+// setInterval(function(){    
+//     for(let i=0;i<gameData.length;i++){
+//         gameData[i]=Math.floor(Math.random()*100);
+//     }
+//     displayData(gameData);
+// },1000);
