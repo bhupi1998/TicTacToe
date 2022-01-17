a59f6a7292c9eee8518e9217b8d74ad3b6146eb9
Bhupinder <bhupi98.bs@gmail.com>
2022-01-16 18:12:34 -0800

    working on updated wincheck funciton

--- script.js
+++ script.js
@@ -4,31 +4,19 @@ const Player = (name,symbol) =>{
     return {name,symbol};
 }
 
-const gameArrays=(gameData) => {
-    let columns=[[0,3,6],[1,4,7],[2,5,8]];
-    let rows=[[0,1,2],[3,4,5],[6,7,8]];
-    let diagonals=[[0,4,8],[2,4,6]];
-    const arrayDataConverter = () =>{
-    for(let i=0;i<3;i++){
-        for(let l=0;l<3;l++){
-            if(rows[i][l]!=undefined)
-                rows[i][l]=gameData[rows[i][l]];
-        }
-    }
-    console.log({rows,columns,diagonals});
-}
-    return {columns,rows,diagonals,arrayDataConverter}
-}
 //trying to use modules to store all functions. This allows to keep the namespace clear.
 const ticTacToe=(()=>{
     let gameData = [];
     let round=0;
     let boxNumber=0;
     let clickIndex=0;
+    const columns=[[0,3,6],[1,4,7],[2,5,8]];
+    const rows=[[0,1,2],[3,4,5],[6,7,8]];
+    const diagonals=[[0,4,8],[2,4,6]];
     //declaring these here. not sure if good idea just yet. Trying to get this working first
     const player1= Player('george','X');
     const player2= Player('jeff','O');
-    const sortedGameData= gameArrays(gameData);
+
     const gridDiv= document.querySelector('#grid');
     let boxList=gridDiv.querySelectorAll(".quadrants");
     //event listeners
@@ -41,9 +29,9 @@ const ticTacToe=(()=>{
         gameControl(player1,player2,clickIndex);
 
     });
-
+    //checks if values in given array are all the same
+    const allEqual = arr => arr.every(v => v === arr[0]); //TODO understand this https://stackoverflow.com/questions/14832603/check-if-all-values-of-array-are-equal
     //Display data updates the grid layout when called
-
     const displayData = (gameData,gridBlockIndex) =>{
         let nodeQ= gridDiv.querySelector(`#${boxList[gridBlockIndex].id}`);
         nodeQ.innerHTML=`${gameData[gridBlockIndex]}`;
@@ -52,75 +40,17 @@ const ticTacToe=(()=>{
     const arrayControl = (symbol,clickIndex) =>{
         gameData[clickIndex]=symbol; 
     }
+//patternChecker will take the data from the game, check it using the patter defined and then return true or false if there is a winner. 
+    const patternChecker=(data,patterToCheck)=>{
+
+    }
     //this function works but not well..
-    const winChecker =(gameData) => {
-        let prev=0;
-        let curr=0;
-        let win=0;
-        let winForReal=0;
-
-
-        const comparator =(current) =>prev===current & current != undefined;
+    const winChecker =(gameData,round) => {
         //check rows
-        sortedGameData.arrayDataConverter();
-        // for(let horiz=0;horiz<3;horiz++){
-        //     prev=gameData[rows[horiz][0]];
-        //     // console.log(rows[horiz].every(comparator)); //this makes no sense.. Rows contains points and not the actual contents of gameData.
-            
-        //        if(win>=2){
-        //             winForReal=1;
-        //             console.log("horiz win by "+ curr);
-        //             break;
-        //        }
-        //     win=0;//reset
-        // }
-
-        // //check columns
-        // prev=0;
-        // curr=0;
-        // win=0;
-        // for(let collum=0;collum<3;collum++){
-        //     prev=gameData[columns[collum][0]];
-        //     for(let columInner=1;columInner<3;columInner++){
-        //         curr=gameData[columns[collum][columInner]];
-        //         if(prev==curr & curr!=undefined){
-        //             win++;
-        //         }else
-        //             win=0;
-        //         if(win>=2){
-        //             winForReal=1;
-        //             console.log("column win by "+ curr);
-        //             break;
-        //         }
-        //         prev=curr;
-        //     }
-        //     win=0;//reset
-        // }
-        // //check diagonals
-        // prev=0;
-        // curr=0;
-        // win=0;
-        // for(let diagz=0;diagz<2;diagz++){
-        //     prev=gameData[diagonals[diagz][0]];
-        //     for(let diagzInner=1;diagzInner<3;diagzInner++){
-        //         curr=gameData[diagonals[diagz][diagzInner]];
-        //         if(prev==curr & curr!=undefined){
-        //             win++;
-        //         }else
-        //             win=0;
-        //         if(win>=2){
-        //             winForReal=1;
-        //             console.log("diagonal win by "+ curr);
-        //             break;
-        //         }
-        //         prev=curr;
-                
-        //     }
-        //     win=0;//reset
-        // }
-        // if(winForReal){
-        //     console.log("you win motherfucker");
-        // }
+        //check diagonals
+        //check columns
+        //declare game outcome
+        //end game
     }
     const gameControl =(player1,player2,clickIndex) =>{
         //control game here
@@ -134,21 +64,10 @@ const ticTacToe=(()=>{
             displayData(gameData,clickIndex); 
         }
         round++;
-        winChecker(gameData);
-        if(round>=9){
-            //check for win.
-            
-            
-        }
+
+        winChecker(gameData,round);
+
     }
-    return {displayData}
+    return {displayData,columns,rows,diagonals}
 })();
 
-//Displays random numbers in the grid
-// setInterval(function(){    
-//     for(let i=0;i<gameData.length;i++){
-//         gameData[i]=Math.floor(Math.random()*100);
-//         displayData(gameData,i);
-//     }
-    
-// },200);
