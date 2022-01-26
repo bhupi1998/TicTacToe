4156015e5448bc36e25a1230cdc38029f2eebc5c
bp <bhupi98.bs@gmail.com>
2022-01-25 21:04:13 -0800

    built evaluation functionality in game control to be used with minimax

--- jsfiles/aiFighter.js
+++ jsfiles/aiFighter.js
@@ -7,8 +7,11 @@ const aiFighter = (()=>{
         do{
             matchOutcome=ticTacToe.gameControl(player1,player2,randomNumber);
             randomNumber=Math.floor(Math.random()*10); 
-        }while(matchOutcome)
-        
+        }while(matchOutcome==1)//matchOutcome!='-' && matchOutcome!=-10 && matchOutcome!=10 && matchOutcome !=0)
+        // possible matchOutcomes: +-10 if a player won
+        //                          0 if it's a tie
+        //                          1 if spot is taken
+        //                          - default. game can carry on
     }
     return {randomMove};
 })();
